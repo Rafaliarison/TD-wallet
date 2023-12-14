79509af5c01965db4ffe14daf0aa40aca97bf1e0
= <=>
2023-12-14 11:34:48 +0300

    init project

---
+++ TD-wallet/src/main/java/com/wallet/tdwallet/TdWalletApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.tdwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdWalletApplication.class, args);
+    }
+
+}
---
+++ TD-wallet/src/test/java/com/wallet/tdwallet/TdWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.tdwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
