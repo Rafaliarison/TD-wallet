f665b6568b40316d6139d758b0130fd1cc85215c
= <=>
2023-12-15 15:23:05 +0300

    feat: add get history of solde between specific time in solderepository

--- TD-wallet/src/main/java/com/wallet/tdwallet/repository/SoldeRepository.java
+++ TD-wallet/src/main/java/com/wallet/tdwallet/repository/SoldeRepository.java
@@ -54,4 +54,32 @@ public class SoldeRepository {
         }
         return soldes;
     }
+
+    public List<Solde> findByIntervalleDate(int accountId, String startDate, String endDate){
+        String sql = "select solde.solde_date, account.account_solde " +
+                "from account " +
+                "join solde on account.account_solde = solde.solde_id " +
+                "where account.account_id = ? AND solde.solde_date BETWEEN ? AND ? " +
+                "order by solde.solde_date";
+        List<Solde> soldeHistory = new ArrayList<>();
+        try (
+             PreparedStatement preparedStatement = dbconnection.prepareStatement(sql)) {
+
+            preparedStatement.setInt(1, accountId);
+            preparedStatement.setString(2, startDate);
+            preparedStatement.setString(3, endDate);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    String date = resultSet.getString("solde_date");
+                    double balance = resultSet.getDouble("account_solde");
+                    soldeHistory.add(new Solde(date, balance));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return soldeHistory;
+    }
 }
