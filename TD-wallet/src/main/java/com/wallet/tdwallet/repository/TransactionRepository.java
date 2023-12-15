24b4628b5b2d7b5b8ada0748d8c79816e036e887
= <=>
2023-12-15 10:12:24 +0300

    feat: make service class for account and solde

---
+++ TD-wallet/src/main/java/com/wallet/tdwallet/repository/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.wallet.tdwallet.repository;
+
+import com.wallet.tdwallet.model.Account;
+import com.wallet.tdwallet.model.Solde;
+
+public class TransactionRepository {
+}
--- TD-wallet/src/main/java/com/wallet/tdwallet/service/TransactionService.java
+++ TD-wallet/src/main/java/com/wallet/tdwallet/service/TransactionService.java
@@ -1,31 +1,27 @@
 package com.wallet.tdwallet.service;
 
+import com.wallet.tdwallet.connection.DBConnection;
 import com.wallet.tdwallet.model.Account;
+import com.wallet.tdwallet.repository.TransactionRepository;
 import lombok.AllArgsConstructor;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 @AllArgsConstructor
 public class TransactionService {
-    Connection connection = new Connection();
-    Connection dbConnect = (Connection) connection.connect();
-    Account accountBeforeTransaction = getAccountDetails(accountId);
-    public Account makeTransaction(int accountid, String transactionLable, double transactionAmount, String transactionType){
+    DBConnection connection = new DBConnection();
+    Connection dbconnection;
+    public Account makeTransaction(int accountId, String transactionLable, double transactionAmount, String transactionType){
         String sql = "insert into transaction(transaction_label, transaction_amount, transaction_type, transaction_date) values (? ,?, ?, now())";
-        try(PreparedStatement preparedStatement = connection.connect().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+        try(PreparedStatement preparedStatement = connection.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
             preparedStatement.setString(1, transactionLable);
             preparedStatement.setDouble(2, transactionAmount);
             preparedStatement.setString(3, transactionType);
             preparedStatement.executeUpdate();
 
-            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
-            if(generatedKeys.next()){
-                int transactionId = generatedKeys.getInt(1);
-                double newBalance = (transactionType.equals("debit")) ? accountBeforeTransaction.getSolde() - transactionAmount : accountBefore.getSolde() + transactionAmount ;
-            }
         }
     }
-
 }
